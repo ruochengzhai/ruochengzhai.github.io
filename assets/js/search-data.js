de69c5943740c928201afe4992ba50b1b81e26f4
ruochengzhai <101622860+ruochengzhai@users.noreply.github.com>
2025-09-24 14:56:14 +0000

    Deploying to gh-pages from @ ruochengzhai/ruochengzhai.github.io@13518c3f5ccfb936a50bd0022502f6804f7e38eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Here&#39;s a copy of my CV.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
